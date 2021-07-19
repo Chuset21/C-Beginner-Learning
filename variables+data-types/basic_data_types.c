4681e2d0c9947d891173d00145366b30971530c9
Miguel <miguelarrieta02@gmail.com>
2021-07-19 19:49:19 +0100

    Added basic_data_types.c

---
+++ variables+data-types/basic_data_types.c
@@ -0,0 +1,12 @@
+#include <stdbool.h>
+
+int main() {
+    float jason = 23.333f;
+    long double jason2 = 55.55555555555l;
+    _Bool boolVariable = 1;
+    // with C99
+    bool boolean = true;
+
+    return 0;
+}
+
