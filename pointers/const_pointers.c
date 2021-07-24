45e86e456bd2ac83be63658fc37ddca0691aaf97
Miguel <miguelarrieta02@gmail.com>
2021-07-24 18:31:39 +0100

    Learned about const keyword in reference to pointers

---
+++ pointers/const_pointers.c
@@ -0,0 +1,8 @@
+int main() {
+    const char *pChar;      // Value cannot be changed through this pointer
+    long *const pLong;      // Pointer address cannot be changed
+
+    const int *const pInt;  // Neither can be changed
+
+    return 0;
+}
