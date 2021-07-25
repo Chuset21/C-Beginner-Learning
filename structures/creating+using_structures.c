7e85a7788800b64227b6877a18c39d534efd6393
Miguel <miguelarrieta02@gmail.com>
2021-07-25 18:28:08 +0100

    Used pointers to structures and in structures

--- structures/creating+using_structures.c
+++ structures/creating+using_structures.c
@@ -25,7 +25,37 @@ int main() {
     } red = {255, 0, 0};
 
     struct mutableDate mutableDate = {9, 25, 2015};
+
+    // This will reset 'year' to the primitive's default value, in this case 0
     mutableDate = (struct mutableDate) {25, 9}; // compound literals
+
+
+    struct mutableDate *pMutableDate = &mutableDate;
+
+    *pMutableDate = (struct mutableDate) {25, 9, 2015}; // compound literals with pointers
+
+    (*pMutableDate).day = 21;
+    pMutableDate->day = 25; // Same thing
+
+    printf("Mutable date = %d/%d/%.2i.\n", pMutableDate->day, pMutableDate->month, pMutableDate->year);
+
+    struct pInts {
+        int *p1;
+        int *p2;
+    } pointers;
+
+    int i1 = 100, i2;
+    pointers = (struct pInts) {&i1, &i2};
+    *pointers.p2 = -97;
+
+    printf("i1 = %i, *pointers.p1 = %i\n", i1, *pointers.p1);
+    printf("i2 = %i, *pointers.p2 = %i\n", i2, *pointers.p2);
+
+    struct pNames {
+        char *first;
+        char *last;
+        int letters;
+    };
 
     return 0;
 }
